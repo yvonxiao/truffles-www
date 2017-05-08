16fd4f84ad07f9a803d64fc53bf1c320e704cefd
Yvon Xiao <yvonxiao@gmail.com>
2017-05-08 23:04:48 +0800

    add test

---
+++ test/models/product.test.js
@@ -0,0 +1,20 @@
+const productModel = require('../../models/product.js')
+
+describe('test/models/product.test.js',function(){
+
+    it('should return a string value in seriesArr',function(){
+        let seriesArr = ['best-seller-series',
+            'row-block-series',
+            'shell-series',
+            'liqueur-series',
+            'wedding-series',
+            'gift-box-series'];
+        seriesArr.indexOf(productModel.getDefaultSeriesParam()).should.not.equal(-1);
+    });
+
+    it('should return productBestSellerSeries which is the series name by the best-seller-series parameter',function(){
+        productModel.getSeriesName('best-seller-series').should.equal('productBestSellerSeries');
+    });
+
+    
+});
